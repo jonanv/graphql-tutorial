9fb8787b2279b4ab0c2a5cb2bd22d2a73309d99e
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-28 16:51:47 -0500

    Se hace peticion send

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 const server = express();
 const PORT = 3000;
 
+server.get('/graphql', (req, res) => {
+    res.send('<html> <head> <body> <h1> Hello word! </h1> </body> </head> </html>');
+});
+
 server.listen(PORT, () => {
     console.log('Listening on port', PORT);
 })

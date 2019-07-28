0efc8604e06ecafc07b3a2701d8cc92b36038152
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-28 17:21:02 -0500

    Se instalo dependencia babel-watch@2.0.7 y se cambio linea babel-watch

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import express from 'express';
 const server = express();
 const PORT = 3000;
 
-server.get('/graphql', (req, res) => {
-    res.send('<html> <head> <title> GraphQL </title> </head> <body> <h1> Hello word! </h1> </body> </html>');
-});
+console.log('Changing the file');
 
 server.listen(PORT, () => {
     console.log('Listening on port', PORT);

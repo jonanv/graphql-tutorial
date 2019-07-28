e55f9438169d775ce0c5b6d47fd6977bfcb4e4b5
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-28 16:56:55 -0500

    Se envió petición con con etiquetas hmtl

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const server = express();
 const PORT = 3000;
 
 server.get('/graphql', (req, res) => {
-    res.send('<html> <head> <body> <h1> Hello word! </h1> </body> </head> </html>');
+    res.send('<html> <head> <title> GraphQL </title> </head> <body> <h1> Hello word! </h1> </body> </html>');
 });
 
 server.listen(PORT, () => {

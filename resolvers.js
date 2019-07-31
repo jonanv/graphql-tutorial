88dec4be2b672678c8e0f8b134bc017e97e24f88
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-07-30 22:10:25 -0500

    Se cambio el parametro age por id para ser consultado por id

--- resolvers.js
+++ resolvers.js
@@ -1,14 +1,17 @@
 const authors = [{
+    id: 21,
     name: 'JK Rowling',
     age: 50,
     books: ['Harry Potter and the Gobelt of Fire', 'Harry Potter and Prisioner of Azkaban']
 },
 {
+    id: 34,
     name: 'George RR Martin',
     age: 60,
     books: ['GOT - Song of Ice and Fire', 'GOT - A Dance with Dragons']
 },
 {
+    id: 12,
     name: 'Stephen King',
     age: 60,
     books: ['The Green Mile', 'Carrie']
@@ -20,8 +23,8 @@ const resolvers = {
         authors: () => {
             return authors
         },
-        author: (root, {age}) => {
-            return authors.find(author => author.age === age);
+        author: (root, {id}) => {
+            return authors.find(author => author.id === id);
         }
     }
 }
--- schemas.js
+++ schemas.js
@@ -1,12 +1,13 @@
 // Construct a schema, using GraphQL schema language
 export default `
     type Author {
+        id: Int
         age: Int
         name: String
         books: [String]
     }
     type Query {
         authors: [Author]
-        author(age: Int): Author
+        author(id: Int): Author
     }
 `;
